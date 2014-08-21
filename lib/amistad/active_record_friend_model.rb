6645d7748ea6bade3e0d3196aaf488e3f4673870
uchenna <jin4rill@yahoo.com>
2014-08-21 17:15:12 +0100

    updated relationship to work with rails4

--- lib/amistad/active_record_friend_model.rb
+++ lib/amistad/active_record_friend_model.rb
@@ -5,56 +5,47 @@ module Amistad
     extend ActiveSupport::Concern
 
     included do
-      #####################################################################################
-      # friendships
-      #####################################################################################
-      has_many  :friendships,
-        :class_name => "Amistad::Friendships::#{Amistad.friendship_model}",
-        :foreign_key => "friendable_id"
+    has_many  :friendships,
+            :class_name => "Amistad::Friendships::#{Amistad.friendship_model}",
+            :foreign_key => "friendable_id"
 
-      has_many  :pending_invited,
-        :through => :friendships,
-        :source => :friend,
-        :conditions => { :'friendships.pending' => true, :'friendships.blocker_id' => nil }
+  has_many  :pending_invited, -> {where('friendships.pending' => true, 'friendships.blocker_id' => nil )},
+            :through => :friendships,
+            :source => :friend
 
-      has_many  :invited,
-        :through => :friendships,
-        :source => :friend,
-        :conditions => { :'friendships.pending' => false, :'friendships.blocker_id' => nil }
+  has_many  :invited, -> {where('friendships.pending' => false, 'friendships.blocker_id' => nil )},
+            :through => :friendships,
+            :source => :friend
 
-      #####################################################################################
-      # inverse friendships
-      #####################################################################################
-      has_many  :inverse_friendships,
-        :class_name => "Amistad::Friendships::#{Amistad.friendship_model}",
-        :foreign_key => "friend_id"
+  #####################################################################################
+  # inverse friendships
+  #####################################################################################
+  has_many  :inverse_friendships,
+            :class_name => "Amistad::Friendships::#{Amistad.friendship_model}",
+            :foreign_key => "friend_id"
 
-      has_many  :pending_invited_by,
-        :through => :inverse_friendships,
-        :source => :friendable,
-        :conditions => { :'friendships.pending' => true, :'friendships.blocker_id' => nil }
+  has_many  :pending_invited_by, -> {where('friendships.pending' => true, 'friendships.blocker_id' => nil )},
+            :through => :inverse_friendships,
+            :source => :friendable
 
-      has_many  :invited_by,
-        :through => :inverse_friendships,
-        :source => :friendable,
-        :conditions => { :'friendships.pending' => false, :'friendships.blocker_id' => nil }
+  has_many  :invited_by, -> {where('friendships.pending' => false, 'friendships.blocker_id' => nil )},
+            :through => :inverse_friendships,
+            :source => :friendable
 
-      #####################################################################################
-      # blocked friendships
-      #####################################################################################
-      has_many  :blocked_friendships,
-        :class_name => "Amistad::Friendships::#{Amistad.friendship_model}",
-        :foreign_key => "blocker_id"
+  #####################################################################################
+  # blocked friendships
+  #####################################################################################
+  has_many  :blocked_friendships,
+            :class_name => "Amistad::Friendships::#{Amistad.friendship_model}",
+            :foreign_key => "blocker_id"
 
-      has_many  :blockades,
-        :through => :blocked_friendships,
-        :source => :friend,
-        :conditions => "friend_id <> blocker_id"
+  has_many  :blockades, -> {where('friend_id <> blocker_id')},
+            :through => :blocked_friendships, 
+            :source => :friend
 
-      has_many  :blockades_by,
-        :through => :blocked_friendships,
-        :source => :friendable,
-        :conditions => "friendable_id <> blocker_id"
+  has_many  :blockades_by, -> {where('friendable_id <> blocker_id')},
+            :through => :blocked_friendships,
+            :source => :friendable 
     end
 
     # suggest a user to become a friend. If the operation succeeds, the method returns true, else false
